399bb66460f2805e9bd58442dd1776e750132a8c
MisanParajuli <150371766+MisanParajuli@users.noreply.github.com>
2024-07-30 00:22:07 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+// Bubble sort
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+
+    vector<int>list = {3,4,5,6,7,8,2,3,5,6,8,1,3,4,7,5,7,5,8,5,8,4,3,6,8,5,3};
+
+    for (int j = 0; j < list.size()-1; j++) {
+        if (list.at(j) >= list[j+1]){
+            for (int i = 0; i < list.size()-1; i++) {
+                if (list[i] >= list[i + 1]) {
+                    swap(list[i], list[i + 1]);
+                };
+            };
+        };
+    };
+
+    for (size_t i = 0; i<list.size(); i++){
+        cout << list.at(i) << endl;
+    };
+
+
+
+
+    return 0;
+}
